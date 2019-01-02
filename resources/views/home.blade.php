d14a10f9582a1eef66c77bdc346db45c3b01f66e
marcoskubas <marcosarobed@gmail.com>
2019-01-02 18:01:54 -0200

    Ativando o VueJS no Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
+<example-component></example-component>
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
